61d56a6c928d44be9914dc5a4e1c801a957f5a45
Engkit Satia Riswara <engkitsatia.riswara@dana.id>
2019-02-12 13:54:15 +0700

    Added second app helper

---
+++ LatianEngkitSecondApp/SecondAppHelper.swift
@@ -0,0 +1,9 @@
+//
+//  SecondAppHelper.swift
+//  LatianEngkitSecondApp
+//
+//  Created by Engkit Satia Riswara on 12/02/19.
+//  Copyright © 2019 Engkit Satia Riswara. All rights reserved.
+//
+
+import Foundation
